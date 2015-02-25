3641880f3efa5bfa4813647d3cece57a648a129c
Alexandre Prates <ajfprates@gmail.com>
2015-02-25 17:03:32 -0300

    Update install script

--- install.sh
+++ install.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
 # Base devel
-pacman -S --needed base-devel
+sudo pacman -S --needed base-devel
+
+# Slim
+sudo pacman -S slim archlinux-themes-slim
 
 # xinitrc
-pacman -S xorg-xrand xorg-setxkbmap
+sudo pacman -S xorg-xrand xorg-setxkbmap
 
 # i3-wm
-pacman -S i3-wm dmenu 
+sudo pacman -S i3-wm dmenu
 
 # i3
-pacman -S xautolock dunst feh terminology
+sudo pacman -S xautolock dunst feh terminology
 
 # lockscreen
-pacman -S scrot imagemagick i3lock
+sudo pacman -S scrot imagemagick i3lock
+
+# Others
+sudo pacman -S thunar gitg
+
+# R7 deps
+sudo pacman -S redis
+
+# Install aurget
+curl https://gist.githubusercontent.com/alexandreprates/b47c6267194465fd888b/raw/572fc53953b8db0eb848712bf61829173643bf63/gistfile1.sh | bash
+
+aurget -S i3blocks  --noedit --deps
+aurget -S imagemagick-no-hdri  --noedit --deps
+aurget -S sublime-text-nightly  --noedit --deps
+aurget -S google-chrome --noedit --deps
+
+aurget -S rabbitmq rabbitmqadmin
