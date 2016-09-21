91592f47eb093f8d4f14d6f0a0a6e69f90389ba2
Alexandre Prates <ajfprates@gmail.com>
2016-09-21 17:38:52 -0300

    sync

--- scripts/toolbox.sh
+++ scripts/toolbox.sh
@@ -1,5 +1,5 @@
 export EDITOR=vim
-export PATH="$PATH:/home/aprates/Workspace/lab/build"
+export PATH="$PATH:/home/aprates/.local/bin"
 
 alias be="bundle exec"
 alias bi="bundle install"
@@ -7,14 +7,13 @@ alias open="xdg-open"
 alias xgitg="gitg . > /dev/null 2>&1 &"
 alias xmeld="meld . > /dev/null 2>&1 &"
 
+alias dc="docker-compose"
+alias reloadtoobox="source ~/.dotfiles/scripts/toolbox.sh"
+
 function checktoolbox {
   echo "Toolbox lodaded!"
 }
 
-# Adding current dir to autojump
-function jadd {
-  j --add `pwd`
-}
 
 # Find for text into ruby/erb files (recursively)
 function codesearch {
@@ -25,14 +24,29 @@ function greenp {
   sed ''/$1/s//`printf "\033[32m$1\033[0m"`/'' $2 ;
 }
 
+# Executa o comando em background no diretorio atual
 function here {
   $1 . > /dev/null 2>&1 &
+}
+
+# chown me all files in dir
+function giveme {
+  sudo chown $USER:$USER *.* -R
+  ls -l
 }
 
 #   Docker functions
 
 function drestart {
   clear && docker stop $1 && docker start -ai $1
+}
+
+function dcrestart {
+  clear && docker-compose stop && docker-compose start && docker-compose logs -f --tail 0
+}
+
+function drspec {
+  clear && docker exec -it $1 bundle exec rspec $2
 }
 
 function dbash {
--- zshrc
+++ zshrc
@@ -10,5 +10,10 @@ export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin"
 source $HOME/.dotfiles/scripts/toolbox.sh
 source $ZSH/oh-my-zsh.sh
 
+# Load rbenv
 PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
+
+
+# Load go2dir
+[[ -s "/home/aprates/.go2dir/go2dir.sh" ]] && source "/home/aprates/.go2dir/go2dir.sh"
