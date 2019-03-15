049bbf87b6566fc1a35c7189e0049732959765b2
Alexandre <alexandre.prates@resultadosdigitais.com.br>
2019-03-15 08:43:11 -0300

    Sync

--- configure.sh
+++ configure.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 ln -sf $HOME/.dotfiles/dunstrc $HOME/.dunstrc
+ln -sf $HOME/.dotfiles/gemrc $HOME/.gemrc
 ln -sf $HOME/.dotfiles/i3 $HOME/.i3
 ln -sf $HOME/.dotfiles/i3/taskbar.conf $HOME/.i3blocks.conf
 ln -sf $HOME/.dotfiles/scripts/lockscreen $HOME/.lockscreen
 ln -sf $HOME/.dotfiles/xinitrc $HOME/.xinitrc
 ln -sf $HOME/.dotfiles/zshrc $HOME/.zshrc
-ln -sf $HOME/.dotfiles/xbindkeysrc $HOME/.xbindkeysrc
+# ln -sf $HOME/.dotfiles/xbindkeysrc $HOME/.xbindkeysrc
 
 echo Config done!
