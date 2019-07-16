5858a8a14be8cfb75247af4480a33bfd3775e106
Alexandre Prates <alexandre.prates@resultadosdigitais.com.br>
2019-07-16 08:05:03 -0300

    Fix music script for utf-8 chars

--- scripts/music.sh
+++ scripts/music.sh
@@ -10,8 +10,8 @@ esac
 TITLE=$(playerctl metadata | grep title | cut -d " " -f 17-)
 ALBUM=$(playerctl metadata | grep album | cut -d " " -f 17-)
 
-TEXT=$(echo "$TITLE - $ALBUM" | iconv -f utf-8 -t ascii//translit)
-TEXT_LENGTH=$(expr length "$TEXT")
+TEXT=$(echo "$TITLE - $ALBUM")
+TEXT_LENGTH=$(echo $TEXT | iconv -f utf-8 -t ascii//translit | wc --chars)
 
 if [ $TEXT_LENGTH -gt $DISPLAY_SIZE ]; then
   SCROLL=$(( $(date +%s) % $(( $TEXT_LENGTH - $DISPLAY_SIZE + 1 )) ))
